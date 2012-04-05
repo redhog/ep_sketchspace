a2b86f9ce54a2d31c3e22162344253d80d037ef5
Egil Moeller <egil.moller@freecode.no>
2012-04-05 19:55:46 +0200

    Automatic initialization

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-cd static/js
+cd "$(dirname $0)/static/js"
 wget http://download.dojotoolkit.org/release-1.6.0/dojo-release-1.6.0-src.tar.gz
+tar -xvzf dojo-release-1.6.0-src.tar.gz
 mv dojo-release-1.6.0-src/* .
 rm -rf dojo-release-1.6.0-src dojo-release-1.6.0-src.tar.gz
--- hooks.js
+++ hooks.js
@@ -1,4 +1,7 @@
 eejs = require("ep_etherpad-lite/node/eejs");
+var util = require('util');
+var child_process = require('child_process');
+var path = require('path');
 
 exports.eejsBlock_editbarMenuLeft = function (hook_name, args, cb) {
   args.content = args.content + eejs.require("ep_sketchspace/templates/sketchSpaceEditbarButtons.ejs");
@@ -18,4 +21,10 @@ exports.eejsBlock_scripts = function (hook_name, args, cb) {
 exports.eejsBlock_styles = function (hook_name, args, cb) {
   args.content = args.content + eejs.require("ep_sketchspace/templates/sketchSpaceStyles.ejs");
   return cb();
+}
+
+exports.init = function(hook_name, args, cb) {
+  child_process.spawn(path.join(__dirname, "build.sh"), [], {customFds: [0, 1, 2]}).on('exit', function (code, signal) {
+    cb();
+  });
 }
